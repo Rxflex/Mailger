365d0b3a6c54c0fd2893cd4d02a87a933797b2da
tuesday <tuesday@weeks.team>
2024-05-24 19:20:07 +0200

    Update dependencies in Cargo.toml and refactor connect_to_imap function.

--- src-tauri/src/imap.rs
+++ src-tauri/src/imap.rs
@@ -1,17 +1,13 @@
 use async_imap::Session;
+use async_native_tls::TlsStream;
 use tokio::net::TcpStream;
-use tokio_native_tls::TlsConnector;
-use native_tls::TlsConnector as NativeTlsConnector;
+use async_native_tls::TlsConnector;
 
-pub async fn connect_to_imap(user: &str, password: &str, imap_server: &str) -> Result<Session<tokio_native_tls::TlsStream<TcpStream>>, Box<dyn std::error::Error>> {
-    let tls_connector = NativeTlsConnector::builder().build()?;
-    let tls_connector = TlsConnector::from(tls_connector);
-
+async fn connect_to_imap(user: &str, password: &str, imap_server: &str) -> Result<Session<TlsStream<TcpStream>>, Box<dyn std::error::Error>> {
+    let tls_connector = TlsConnector::new();
     let tcp_stream = TcpStream::connect((imap_server, 993)).await?;
     let tls_stream = tls_connector.connect(imap_server, tcp_stream).await?;
-
-    let client = async_imap::connect((imap_server, 993), imap_server, tls_stream).await?;
+    let client = async_imap::connect((imap_server, 993), tls_stream).await?;
     let session = client.login(user, password).await.map_err(|e| e.0)?;
-
     Ok(session)
 }
