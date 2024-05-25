b43bd711d3175710d85aa0a2623f6850cffa3a71
tuesday <tuesday@weeks.team>
2024-05-25 19:39:24 +0200

    Update page preload.js to handle input data and display alerts.

--- src/pages-handler/login.js
+++ src/pages-handler/login.js
@@ -1,4 +1,7 @@
 // src\pages-handler\login.js
-module.exports = function (mainWindow, event, ...args) {
-    console.log('hello from login');
+module.exports = function (mainWindow, ipcMain, event, ...args) {
+    const inputs = JSON.parse(args[0][0].document);
+    if (!inputs.imap_server || !inputs.imap_login || !inputs.imap_password || !inputs.smtp_server || !inputs.smtp_login || !inputs.smtp_password) return mainWindow.webContents.send('alert', 'You need to fill in all the fields!');
+
+
 }
--- src/pages/preload.js
+++ src/pages/preload.js
@@ -1,5 +1,19 @@
-const { ipcRenderer } = require("electron");
+const { ipcRenderer, dialog } = require("electron");
 
 function sendToBackend(page) {
-    ipcRenderer.send(page, {value: JSON.stringify(document)});
+    let inputsraw, index;
+    const inputs = {};
+    inputsraw = document.getElementsByTagName('input');
+    for (index = 0; index < inputsraw.length; ++index) {
+        inputs[inputsraw[index].id] = inputsraw[index].value
+    }
+    ipcRenderer.send(page, {document: JSON.stringify(inputs)});
 }
+
+ipcRenderer.on('response', (event, message) => {
+    eval(message);
+})
+
+ipcRenderer.on('alert', (event, message) => {
+    alert(message);
+})
